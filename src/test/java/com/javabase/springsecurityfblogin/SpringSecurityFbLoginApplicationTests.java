66747abc1931196abd03943649d9eb7556c2d3f3
p.singh <p.singh@synchronoss.com>
2020-02-19 19:24:41 +0530

    oauth2 demo

---
+++ src/test/java/com/javabase/springsecurityfblogin/SpringSecurityFbLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javabase.springsecurityfblogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFbLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
